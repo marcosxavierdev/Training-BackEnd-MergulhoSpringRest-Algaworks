bbe77db2dcb0ece2480ea5c30c114af9ec3a9810
Marcos.Xavier <@marcosxavierdev>
2021-08-22 11:18:30 -0300

    configurações iniciais

---
+++ msr-logistica-api/src/main/java/br/com/msr/logistica/MsrLogisticaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.msr.logistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsrLogisticaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsrLogisticaApiApplication.class, args);
+	}
+
+}
---
+++ msr-logistica-api/src/test/java/br/com/msr/logistica/MsrLogisticaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.msr.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsrLogisticaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
